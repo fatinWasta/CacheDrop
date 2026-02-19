6cc168ae25cd079473fa207fb1f2ba3f5a043b56
fatin41 <fatin@particle41.com>
2026-02-19 14:32:29 +0530

    Initial Commit

---
+++ CacheDrop/CacheDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheDropApp.swift
+//  CacheDrop
+//
+//  Created by Fatin on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CacheDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CacheDropTests/CacheDropTests.swift
@@ -0,0 +1,17 @@
+//
+//  CacheDropTests.swift
+//  CacheDropTests
+//
+//  Created by Fatin on 19/02/26.
+//
+
+import Testing
+@testable import CacheDrop
+
+struct CacheDropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
